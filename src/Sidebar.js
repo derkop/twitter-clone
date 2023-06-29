c1ed39bec7a5c4f332566076f92c7a35d07f68ed
Derrick <dejko@ucsc.edu>
2023-06-29 01:33:40 -0700

    sidebar done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,24 +1,32 @@
-import react from "react";
-import './Sidebar.css'
-import HomeIcon from '@mui/icons-material/Home';
+import "./Sidebar.css";
 import TwitterIcon from '@mui/icons-material/Twitter';
+import SidebarOption from "./SidebarOptions";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Button from '@mui/material/Button';
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            
-            {/* Twitter icon */}
-            <TwitterIcon />
+            <TwitterIcon className="twitter__icon"/>
 
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
-            {/* Sidebar Option */}
+            <SidebarOption active Icon={HomeIcon} text="Home" />
+            <SidebarOption Icon={SearchIcon} text="Explore" />
+            <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+            <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+            <SidebarOption Icon={ListAltIcon} text="Lists" />
+            <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+            <SidebarOption Icon={MoreHorizIcon} text="More" />
 
-            {/* Tweet Button */}
-            
+            <Button variant="outlined" className="sidebar__tweet" fullWidth disableRipple> Tweet</Button>
             
 
 
