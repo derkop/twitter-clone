ebdb276bdcc9077da46d7bcb62aaa239d2a22d03
derrick ubuntu <dejko@ucsc.edu>
2023-07-09 02:24:03 -0700

    working on firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,18 @@
 import "./Feed.css";
 import Post from "./Post";
 import TweetBox from "./TweetBox";
+import React, {useEffect, useState} from 'react';
+// import db from './firebase';
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   db.collection('posts').onSnapshot(snapshot => (
+  //     setPosts(snapshot.docs.map(doc => doc.data()))
+  //   ))
+  // }, []);
+
   return (
     <div className="feed">
       <div className="feed__header">
@@ -11,11 +21,25 @@ function Feed() {
       
     <TweetBox/>
 
+
+    {posts.map(post => (
+      <Post
+      displayName={post.displayName}
+      username={post.username}
+      verified={post.verified}
+      text={post.text}
+      avatar={post.avatar}
+      image={post.image}
+      />
+    ))}
+
     <Post 
     displayName="Derrick Ko"
     username="derrick.ko"
     verified={true}
     text= "HIII KIMMY"
+    avatar="https://i.imgur.com/TOQO1Bz.jpg"
+    image="https://media4.giphy.com/media/LpoT1DojgcyW9QrCil/giphy.gif?cid=ecf05e47rl9pr66asj9rvoew4cqp5u16v6egowrl6e7gtmg3&ep=v1_gifs_search&rid=giphy.gif&ct=g"
     />
 
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,15 @@
-import firebase from "firebase";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "@firebase/firestore"
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCNaDpFpeNCfrWPuEvBAJIwQTKvNbuKtjo",
-    authDomain: "twitter-clone-9caf6.firebaseapp.com",
-    projectId: "twitter-clone-9caf6",
-    storageBucket: "twitter-clone-9caf6.appspot.com",
-    messagingSenderId: "231119820005",
-    appId: "1:231119820005:web:3adee4064361a23990700c",
-    measurementId: "G-C7J1YVYE49"
-  };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCNaDpFpeNCfrWPuEvBAJIwQTKvNbuKtjo",
+//   authDomain: "twitter-clone-9caf6.firebaseapp.com",
+//   projectId: "twitter-clone-9caf6",
+//   storageBucket: "twitter-clone-9caf6.appspot.com",
+//   messagingSenderId: "231119820005",
+//   appId: "1:231119820005:web:3adee4064361a23990700c",
+//   measurementId: "G-C7J1YVYE49"
+// };
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-  const db = firebaseApp.firestore();
-  
-  export default db;
+// const app = initializeApp(firebaseConfig);
+// export const firestore = getFirestore(app)
