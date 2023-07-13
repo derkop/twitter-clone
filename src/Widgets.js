2482865a0b7bcfc75cd7edeb4500c6ccf8083348
derrick ubuntu <dejko@ucsc.edu>
2023-07-12 22:25:03 -0700

    widget links

--- src/Widgets.js
+++ src/Widgets.js
@@ -19,6 +19,24 @@ function Widgets() {
         <h2>What's happening</h2>
         <TwitterShareButton/>
       </div>
+
+      <footer className="footer">
+      <div className="footer-links">
+        <div className="top-links">
+          <a href="https://twitter.com/en/tos" target="_blank">Terms of Service</a>&nbsp;&nbsp;
+          <a href="https://twitter.com/privacy" target="_blank">Privacy Policy</a>&nbsp;&nbsp;
+          <a href="https://help.twitter.com/en/rules-and-policies/twitter-cookies" target="_blank">Cookie Policy</a>
+        </div>
+        <div className="bottom-links">
+          <a href="https://help.twitter.com/resources/accessibility" target="_blank">Accessibility</a>&nbsp;&nbsp;
+          <a href="https://help.twitter.com/en/twitter-for-business/ads-info" target="_blank">Ads Info</a>
+        </div>
+      </div>
+    </footer>
+
+      <p className="footer-info">
+        &copy; 2023 Twitter, Inc.
+      </p>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC6JSJ1worVkmOpOrqrROhZe1SBVZdGxL0",
@@ -13,5 +14,25 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+// export const auth = getAuth(app);
+
+// const provider = new GoogleAuthProvider();
+
+// export const signInWithGoogle = () => {
+//   signInWithPopup(auth, provider)
+//     .then((result) => {
+//       const name = result.user.displayName;
+//       const email = result.user.email;
+//       const profilePic = result.user.photoURL;
+
+//       localStorage.setItem("name", name);
+//       localStorage.setItem("email", email);
+//       localStorage.setItem("profilePic", profilePic);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+//   }
 
 export default db;
