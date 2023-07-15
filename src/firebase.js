52bfb5099fef935b8ee67bd45b3c5f80c57d91a1
derrick ubuntu <dejko@ucsc.edu>
2023-07-14 17:14:30 -0700

    google login added

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,25 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+      const profilePic = result.user.photoURL;
+
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("profilePic", profilePic);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 
 export default db;
--- src/login/Login.js
+++ src/login/Login.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './Login.css';
 import CloseIcon from '@mui/icons-material/Close';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import {createButton} from "react-social-login-buttons";
-import { GoogleLoginButton } from "react-social-login-buttons";
+import GoogleLogo from '../assets/GoogleLogo.png';
+import AppleLogo from '../assets/AppleLogo.png';
+import { signInWithGoogle } from "../firebase";
 
 function Box() {
 
@@ -14,9 +15,21 @@ function Box() {
                 <div className='Text'>
                     <h3>Sign in to Twitter</h3>
                 </div>
-                
-                <GoogleLoginButton className="google__signin" onClick={() => alert("Hello")} />
-                <button className="sign-in-button">Sign in with Apple</button>
+
+                <button className="google__button" onClick={signInWithGoogle} >
+                <img src={GoogleLogo}/>
+                <span>Sign in with Google</span>
+                </button>
+
+
+                <button className="apple__button">
+                <img src={AppleLogo}/>
+                <span>Sign in with Apple</span>
+                </button>
+                <p> or</p>
+                <button className="email__button">
+                Sign in with phone or email
+                </button>
                 <CloseIcon className="closeButton" id="closeButton">Close</CloseIcon>
             </div>
         </div>
